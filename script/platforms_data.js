52a9e32bf37e9257dfc5ece3ec08140ab952d408
Vlad Verba <vv2298@columbia.edu>
2023-02-09 13:52:06 -0500

    removed feed boost and reel bost

--- script/platforms_data.js
+++ script/platforms_data.js
@@ -1,33 +1,31 @@
 /*
-
 Store the platforms data dictionary here.
 Store lowCPM, avgCPM, highCPM, avgOrganicImpressions for each platform.
 This is used to calculate the cost for each platform.
 
 NOTE: Currently hard coded, but likely will need to be replaced with a database
 that has a collection of out social platforms and their data.
-
 */
 
 const PLATFORMS_DATA_DICT = {
-  "Instagram - Overall": {
+  "Instagram - Public": {
     lowCPM: 2.46,
     avgCPM: 7.76,
     highCPM: 13.06,
     avgOrganicImpressions: 17055,
   },
-  "Instagram - Feed Boost": {
-    lowCPM: 2.46,
-    avgCPM: 5.0,
-    highCPM: 7.55,
-    avgOrganicImpressions: 16669,
-  },
-  "Instagram - Reel Boost": {
-    lowCPM: 8.17,
-    avgCPM: 9.91,
-    highCPM: 12.56,
-    avgOrganicImpressions: 23439,
-  },
+  // "Instagram - Feed Boost": {
+  //   lowCPM: 2.46,
+  //   avgCPM: 5.0,
+  //   highCPM: 7.55,
+  //   avgOrganicImpressions: 16669,
+  // },
+  // "Instagram - Reel Boost": {
+  //   lowCPM: 8.17,
+  //   avgCPM: 9.91,
+  //   highCPM: 12.56,
+  //   avgOrganicImpressions: 23439,
+  // },
   "Instagram - Dark Post": {
     lowCPM: 1.3,
     avgCPM: 1.64,
